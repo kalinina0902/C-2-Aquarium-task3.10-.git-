7b01b29adfe8298a8fc28c7a6e456ab6aac3c3ae
kalinina0902 <ann.kalinina99@yandex.ru>
2017-11-28 23:19:39 +0300

    1

--- Aquarium(task3.10)/FishStrategy.cs
+++ Aquarium(task3.10)/FishStrategy.cs
@@ -53,42 +53,45 @@ namespace Aquarium_task3._10_
         }
         public void Eating(Fish f, List<Eat> Feed)
         {
-            int min;
-            foreach (Eat ea in Feed)
-                foreach (Eat.Food kr in ea.Korm)
-                {
+            //int min;
+            //foreach (Eat ea in Feed)
+            //    foreach (Eat.Food kr in ea.Korm)
+            //    {
 
-                }
-
-            double dx = x - ;
-            double dy = y - e.Y;
-            while (x != e.X && y != e.Y)
+            //    }
+            Random rnd = new Random();
+            int i = rnd.Next(0, Feed.Count);
+            double dx = f.X - Feed[i].Korm[i].x;
+            double dy = f.Y - Feed[i].Korm[i].y;
+            while (dx != 0 && dy != 0)
             {
                 if (dx < 0)
                 {
-                    x += dx / 10.0;
+                    f.X += dx / 10.0;
                 }
                 else if (dx == 0)
                 {
-                    x += 0;
+                    f.X += 0;
                 }
                 else
                 {
-                    x -= dx / 10.0;
+                    f.X -= dx / 10.0;
                 }
                 if (dy < 0)
                 {
-                    y += dy / 10.0;
+                    f.Y += dy / 10.0;
                 }
                 else if (dy == 0)
                 {
-                    y += 0;
+                    f.Y += 0;
                 }
                 else
                 {
-                    y -= dy / 10.0;
+                    f.Y -= dy / 10.0;
                 }
             }
+            Feed.RemoveAt(i);
+
 
 
         }
